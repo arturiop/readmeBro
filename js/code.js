e257c296a297db66ccdaff0870765944d159cb27
Artur <arturioup@gmail.com>
2021-01-06 10:07:21 +0200

    task5

--- js/code.js
+++ js/code.js
@@ -1,41 +1,20 @@
-// task 4.0
+// task 5.0
+"use strict"
+let userLogin = "Admin";
+let userPassword = "ffgg05";
+let enteredUserLogin = prompt("write login", "");
 
-let a = prompt("Первое число?", 1);
-let b = prompt("Второе число?", 2);
-
-alert(+a + +b); // 12
-
-// task 4.1
-
-let useranswer = prompt("what oficial name JS?", "");
-let trueAnswer = "ECMAScript";
-if (useranswer.toUpperCase == trueAnswer.toUpperCase) {
-	alert("wonderful");
+if (userLogin === enteredUserLogin) {
+	let enterefUserPassword = prompt("write password", "");
+	if (enterefUserPassword === userPassword) {
+		alert("hi boss");
+	} else if (enterefUserPassword == null) {
+		alert("canceling");
+	} else {
+		alert("not correct pessword");
+	}
+} else if (enteredUserLogin == null) {
+	alert("canceling");
 } else {
-	alert("go lean my friends")
-
+	alert("indefinitely");
 }
-
-// task 4.2
-
-let userNumb = prompt("write number now!", " ");
-let trueNumb = 1;
-let falseNumb = -1;
-let zero = 0;
-if (userNumb > 0) {
-	alert(trueNumb);
-} else if (userNumb < 0) {
-	alert(falseNumb);
-} else if (userNumb == 0) {
-	alert(zero);
-}
-
-// task 4.3
-
-let result = (a + b < 4) ? "this few" : "this many";
-
-// task 4.4
-let message = (login == "emploer") ? "Hello" :
-	(login == "director") ? "Hello sir" :
-		(login == " ") ? "not login" : " ";
-
