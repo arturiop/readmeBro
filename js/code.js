5b5728be830d11c2de01a45919b342da3b967cce
Artur <arturioup@gmail.com>
2021-01-17 15:05:09 +0200

    task18

--- js/code.js
+++ js/code.js
@@ -1,73 +1,51 @@
 "use strict"
-//task 17
+//task 18
 
-let f = new Function('a', 'return a+1');
-
-function f(b, a) {
-	console.log(a + b);
-}
-setTimeout(f, 10000, "5", "4");
-let timerId = setInterval(() => console.log('tick'), 1000);
-setTimeout(() => { clearInterval(timerId); console.log('stop'); }, 50000);
-
-let timeout = 1000;
-setTimeout(function f() {
-	let i = 0;
-	i++;
-	console.log(i);
-	setTimeout(f, timeout);
-}, timeout);
-
-
-
-
-
-// task 17.1
-function printNumbers(from, to) {
-	setTimeout(() => {
-		if (from <= to) {
-			console.log(from);
-			from += printNumbers(from + 1, to);
-		}
-	}, 1000);
-
-}
-printNumbers(1, 10);
-
-
-
-
-//task17.3
-function sayHi() {
-	console.log(this.name);
-}
-
-let user = { name: "John" };
-let admin = { name: "Admin" };
-
-sayHi.call(user); // John
-sayHi.call(admin); // Admin
-
-
-let st = Object.getOwnPropertyDescriptors(Math);
-console.log(st);
-
-
-
-
-//task17.5
-let user = {
-	name: "John",
-	surname: "Smith",
+let artur = {
+	name: "artur",
+	age: 26,
 
 	get fullName() {
-		return `${this.name} ${this.surname}`;
+		if (this.surName == null) {
+			return `${this.name} Piloian`;
+		} else {
+			return `${this.name} ${this.surName}`;
+		}
 	},
 
 	set fullName(value) {
-		this.name = value;
+		this.surName = value;
 	}
+}
+
+
+console.log(artur.fullName);
+artur.fullName = "iam";
+console.log(artur.fullName);
+
+
+
+//task18.1
+let head = {
+
+	glasses: 1
 };
 
-user.fullName = "Alice Cooper";
-console.log(`${user.name} ${user.surname}`);
+let table = {
+	__proto__: head,
+	pen: 3
+};
+
+let bed = {
+	__proto__: table,
+	sheet: 1,
+	pillow: 2
+};
+
+let pockets = {
+	__proto__: bed,
+	money: 2000
+};
+
+
+
