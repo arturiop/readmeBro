dac541b3a6dc0feac1b7eb7ffbb347ae50c90f7f
Artur <arturioup@gmail.com>
2021-01-23 09:04:29 +0200

    task21

--- js/code.js
+++ js/code.js
@@ -1,77 +1,97 @@
+"use stric"
+//task 21
 
-//task 20
-let er = new SyntaxError("eroorina");
 
-let json = '{ "age": 30 }';
-try {
-	let use = JSON.parse(json);
 
-	if (!use.name) {
-		throw new SyntaxError("lfyyst");
+// let prommise = new Promise((resolve, reject) => {
+// 	setTimeout(() => resolve("fds"), 2000);
+// });
+// prommise.finally(() => console.log("fuction load"))
+// prommise.then(console.log);
+// prommise.catch(console.log);
+
+
+
+
+
+
+// new Promise((resolve, reject) => {
+// 	console.log("1");
+// 	resolve("ок");
+// }).then((result) => {
+// 	throw new Error("Ошибка!"); // генерируем ошибку
+// }).then((result) => {
+// 	console.log("2"); // генерируем ошибку
+// }).catch(console.log);
+
+
+
+
+
+
+// new Promise(function () {
+// 	noSuchFunction(); // Ошибка (нет такой функции)
+// })
+// 	.then(() => {
+
+// 	}); 
+
+
+// async function loadJson(url) {
+// 	let response = await fetch(url);
+
+// 	if (response.status == 200) {
+// 		let json = await response.json();
+// 		return json;
+// 	}
+// 	throw new Error(response.status);
+
+// }
+
+
+// loadJson('no-such-user.json') // (3)
+// 	.catch(console.log); 
+
+
+
+
+class HttpError extends Error {
+	constructor(response) {
+		super(`${response.status} for ${response.url}`);
+		this.name = 'HttpError';
+		this.response = response;
 	}
-	console.log(use.name);
-
-} catch (er) {
-	console.log(er.message);
 }
 
+async function loadJson(url) {
+	let response = fetch(url);
 
-
-// task 20.1
-class MyErrorClass extends Error {
-	constructor(message) {
-		super(message);
-		this.name = "ValidationError";
-	}
-}
-
-class ValidationError extends MyErrorClass {
-
-}
-
-class PropertyRequiredError extends ValidationError {
-	constructor(property) {
-		super("Not property:" + property);
-		this.property = property;
-		this.name = "PropertyRequiredError";
-	}
-}
-
-// Использование
-function readUser(json) {
-	let user = JSON.parse(json);
-
-	if (!user.age) {
-		throw new PropertyRequiredError("age");
-	}
-	if (!user.name) {
-		throw new PropertyRequiredError("name");
-	}
-
-	return user;
-}
-
-// Рабочий пример с try..catch
-
-try {
-	let user = readUser('{"name": "ff" }');
-} catch (err) {
-	if (err instanceof ValidationError) {
-		console.log(err.message);
-		console.log(err.property);
-		console.log(err.name);
-	} else if (err instanceof SyntaxError) { // (*)
-		console.log('');
-		("JSON Ошибка Синтаксиса: " + err.message);
+	if (response.status == 200) {
+		return response.json();
 	} else {
-		throw err;
+		throw new HttpError(response);
 	}
+})
 }
 
+// Запрашивать логин, пока github не вернёт существующего пользователя.
+async function demoGithubUser() {
 
-class FormatError extends SyntaxError {
-	constructor(message) {
-		this.name = "FormatError";
-		super(message);
-	}
+	let name = prompt("Введите логин?", "iliakan");
+
+	let response = loadJson(`https://api.github.com/users/${name}`);
+	let user = await (user => {
+		console.log(`Полное имя: ${user.name}.`);
+		return user;
+	})
+		.catch(err => {
+			if (err instanceof HttpError && err.response.status == 404) {
+				alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+				return demoGithubUser();
+			} else {
+				throw err;
+			}
+		});
 }
+
+demoGithubUser();
