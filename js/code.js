58cccc350d7478a68dfa90127d7b9577b77f4356
Artur <arturioup@gmail.com>
2021-01-26 10:49:39 +0200

    task7

--- js/code.js
+++ js/code.js
@@ -1,18 +1,27 @@
 "use stric"
 
-function list(names) {
-	let str = "";
+function comp(array1, array2) {
+	//your code here
+	if (array1.length == 0) {
+		return null;
+	}
 
-	for (let i = 0; i < names.length; i++) {
-		if (i == names.length - 2) {
-			str += (names[i]).name + " & ";
-		} else if (i == names.length - 1) {
-			str += (names[i]).name + "";
-		} else {
-			str += (names[i]).name + ", ";
-		}
+	let result = true;
+	for (let i = 0; i < array2.length; i++) {
+		array2[i] = Math.sqrt(array2[i]);
 	};
-	return str;
-};
+	array2.filter(function (item) {
 
-console.log(list([{ name: 'Bart' }, { name: 'Lisa' }]));
+		if (array1.includes(item) == false) {
+			result = false;
+		}
+	});
+
+	return result;
+
+
+};
+a1 = [121, 144, 11, 19, 161, 19, 144, 19];
+a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19];
+
+console.log(comp(a1, a2));
