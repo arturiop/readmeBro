f8b0e34fde44065a6649c34eb392975d5003f21f
Artur <arturioup@gmail.com>
2021-01-26 11:15:27 +0200

    task8

--- js/code.js
+++ js/code.js
@@ -1,27 +1,29 @@
 "use stric"
 
-function comp(array1, array2) {
-	//your code here
-	if (array1.length == 0) {
-		return null;
+function findOutlier(integers) {
+	//your code herefor
+	let c = 0;
+	let nc = 0;
+	for (let key in integers) {
+		if (integers[key] % 2 == 0) {
+			c++;
+			// console.log(integers[key]);
+		} else nc++;
 	}
+	let whichArray = (c > nc) ? c : nc;
+	if (whichArray == c) {
+		for (let key in integers) {
+			if (integers[key] % 2 !== 0) {
+				return integers[key];
 
-	let result = true;
-	for (let i = 0; i < array2.length; i++) {
-		array2[i] = Math.sqrt(array2[i]);
-	};
-	array2.filter(function (item) {
-
-		if (array1.includes(item) == false) {
-			result = false;
+			}
 		}
-	});
+	} else for (let key in integers) {
+		if (integers[key] % 2 == 0) {
+			return integers[key];
 
-	return result;
-
-
-};
-a1 = [121, 144, 11, 19, 161, 19, 144, 19];
-a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19];
-
-console.log(comp(a1, a2));
+		}
+	}
+}
+console.log(findOutlier([0, 1, 2]));
+console.log(findOutlier([1, 1, 0, 1, 1]));
