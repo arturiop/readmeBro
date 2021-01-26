a7434a2ce51ac44d3857eb2bf59adbeddf74b35b
Artur <arturioup@gmail.com>
2021-01-26 09:09:39 +0200

    task6

--- js/code.js
+++ js/code.js
@@ -1,34 +1,18 @@
 "use stric"
-//task 21
 
-function spinWords(str) {
+function list(names) {
+	let str = "";
 
-	let newArray = str.split(" ");
-
-	if (newArray.length > 1) {
-		for (let i = 0; i < newArray.length; i++) {
-			let wi = Array.from(newArray[i]);
-
-			if (wi.length > 4) {
-				wi.reverse();
-				newArray[i] = wi.join("");
-			} else {
-				newArray[i] = wi.join("");
-			}
+	for (let i = 0; i < names.length; i++) {
+		if (i == names.length - 2) {
+			str += (names[i]).name + " & ";
+		} else if (i == names.length - 1) {
+			str += (names[i]).name + "";
+		} else {
+			str += (names[i]).name + ", ";
 		}
-		newArray = newArray.join(" ");
-	} else {
-		let ww = Array.from(newArray[0]);
-		ww.reverse();
-		newArray = ww.join("");
-	}
+	};
+	return str;
+};
 
-	return newArray;
-
-}
-
-console.log(spinWords("Hey fellow warriors"));
-console.log(spinWords("Welcome"));
-console.log(spinWords("Hey fellow warriors"));
-console.log(spinWords("This is a test"));
-console.log(spinWords("This is another test"));
+console.log(list([{ name: 'Bart' }, { name: 'Lisa' }]));
