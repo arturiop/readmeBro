dcbe23375374517f61756944a5f1cab46906f0e6
Artur <arturioup@gmail.com>
2021-01-27 10:17:03 +0200

    task11

--- js/code.js
+++ js/code.js
@@ -1,115 +1,28 @@
 "use stric"
 
-function solution(number) {
-	let ourArray = String(number).split("");
-	let cont = 0;
-	let result = [];
+function generateHashtag(str) {
+	if (str.length == 0) {
+		return false;
+	}
+	let result = generate(str);
 
-	for (let i = ourArray.length - 1; i >= 0; --i) {
+	if ((result.length <= 140) && ((result.length > 1))) {
+		return result;
+	} return false;
 
-		++cont;
-		if (cont == 1) {
-			result.unshift(createNum(+ourArray[i]));
-		} else if (cont == 2) {
-			result.unshift(createTens(+ourArray[i]));
-		} else if (cont == 3) {
-			result.unshift(createHundreds(+ourArray[i]));
-		} else if (cont == 4) {
-			result.unshift(createThousand(+ourArray[i]));
+
+
+
+
+	function generate(str) {
+		let res = str.split(" ");
+		for (let key in res) {
+			res[key] = res[key].charAt(0).toUpperCase() + res[key].slice(1);
 		}
-	};
+		let result = "#" + res.join("");
+		return result;
+	}
 
-	return console.log(result.join(""));
-
-
-
-	function createNum(str) {
-		switch (str) {
-			case 1:
-				return "I";
-			case 2:
-				return "II";
-			case 3:
-				return "III";
-			case 4:
-				return "IV";
-			case 5:
-				return "V";
-			case 6:
-				return "VI";
-			case 7:
-				return "VII";
-			case 8:
-				return "VIII";
-			case 9:
-				return "IX";
-		}
-	};
-	function createTens(str) {
-		switch (str) {
-			case 1:
-				return "X";
-			case 2:
-				return "XX";
-			case 3:
-				return "XXX";
-			case 4:
-				return "XL";
-			case 5:
-				return "L";
-			case 6:
-				return "LX";
-			case 7:
-				return "LXX";
-			case 8:
-				return "LXXX";
-			case 9:
-				return "XC";
-		}
-	};
-	function createHundreds(str) {
-		switch (str) {
-			case 1:
-				return "C";
-			case 2:
-				return "CC";
-			case 3:
-				return "CCC";
-			case 4:
-				return "CD";
-			case 5:
-				return "D";
-			case 6:
-				return "DC";
-			case 7:
-				return "DCC";
-			case 8:
-				return "DCCC";
-			case 9:
-				return "CM";
-		}
-	};
-	function createThousand(str) {
-		switch (str) {
-			case 1:
-				return "M";
-			case 2:
-				return "MM";
-			case 3:
-				return "MMM";
-			case 4:
-				return "MMMM";
-		}
-	};
 };
 
-
-solution(22);
-solution(19);
-solution(11);
-solution(10);
-solution(5);
-solution(121);
-solution(2);
-
-
+console.log(generateHashtag("codewars is nice"));
