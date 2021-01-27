f5f9de28103b55b3e4c43077b7e7789b949850de
Artur <arturioup@gmail.com>
2021-01-27 08:42:40 +0200

    task10

--- js/code.js
+++ js/code.js
@@ -1,17 +1,115 @@
 "use stric"
 
-function scramble(str1, str2) {
+function solution(number) {
+	let ourArray = String(number).split("");
+	let cont = 0;
+	let result = [];
 
-	let result;
-	for (let i = 0; i < str2.length; i++) {
+	for (let i = ourArray.length - 1; i >= 0; --i) {
 
-		if (str1.indexOf(str2[i]) == -1) {
-			result = false;
-			break;
+		++cont;
+		if (cont == 1) {
+			result.unshift(createNum(+ourArray[i]));
+		} else if (cont == 2) {
+			result.unshift(createTens(+ourArray[i]));
+		} else if (cont == 3) {
+			result.unshift(createHundreds(+ourArray[i]));
+		} else if (cont == 4) {
+			result.unshift(createThousand(+ourArray[i]));
 		}
-		result = true;
-	}
-	return result;
+	};
 
+	return console.log(result.join(""));
+
+
+
+	function createNum(str) {
+		switch (str) {
+			case 1:
+				return "I";
+			case 2:
+				return "II";
+			case 3:
+				return "III";
+			case 4:
+				return "IV";
+			case 5:
+				return "V";
+			case 6:
+				return "VI";
+			case 7:
+				return "VII";
+			case 8:
+				return "VIII";
+			case 9:
+				return "IX";
+		}
+	};
+	function createTens(str) {
+		switch (str) {
+			case 1:
+				return "X";
+			case 2:
+				return "XX";
+			case 3:
+				return "XXX";
+			case 4:
+				return "XL";
+			case 5:
+				return "L";
+			case 6:
+				return "LX";
+			case 7:
+				return "LXX";
+			case 8:
+				return "LXXX";
+			case 9:
+				return "XC";
+		}
+	};
+	function createHundreds(str) {
+		switch (str) {
+			case 1:
+				return "C";
+			case 2:
+				return "CC";
+			case 3:
+				return "CCC";
+			case 4:
+				return "CD";
+			case 5:
+				return "D";
+			case 6:
+				return "DC";
+			case 7:
+				return "DCC";
+			case 8:
+				return "DCCC";
+			case 9:
+				return "CM";
+		}
+	};
+	function createThousand(str) {
+		switch (str) {
+			case 1:
+				return "M";
+			case 2:
+				return "MM";
+			case 3:
+				return "MMM";
+			case 4:
+				return "MMMM";
+		}
+	};
 };
-console.log(scramble('rkqodlw', 'worsld'));
+
+
+solution(22);
+solution(19);
+solution(11);
+solution(10);
+solution(5);
+solution(121);
+solution(2);
+
+
